5dcd68b9d431ff6edf41e533c20f2385f827ce1c
Zombie <erronblackX9@gmail.com>
2023-04-22 19:25:14 +0600

    'init'

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,7 @@ import (
 func (app *application) routes() http.Handler {
 
 	router := httprouter.New()
-
+	//middlewareXX99
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
